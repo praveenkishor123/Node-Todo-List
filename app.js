f435c6e5297dea1a148d4bfc5ced66237efca4db
Praveen Kishor <praveen.kishor123@gmail.com>
2020-01-13 10:27:21 +0530

    adding nodemon

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 console.log('Running app.js');
 
 const fs = require('fs');
+const _ = require('lodash');
 
 const todos = require('./todos.js');
 
 console.log(todos.addTodo());
+console.log('We Love Nodemon');
+
 
